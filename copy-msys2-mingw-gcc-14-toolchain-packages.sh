dc00914c71e90610f38fa4ec904cca250cfd860b
Tim Stahlhut <stahta01@gmail.com>
2024-10-12 23:40:57 -0400

    script: Add copy-msys2-mingw-gcc-14-toolchain-packages.sh

---
+++ copy-msys2-mingw-gcc-14-toolchain-packages.sh
@@ -0,0 +1,32 @@
+mkdir -p packages/gcc14_toolchain_msys2_mingw64_packages
+
+rm -f ./packages/gcc14_toolchain_msys2_mingw64_packages/*.zst
+cp -f ./mingw-w64-cygwin-like-cross-binutils/mingw-w64-cygwin-like-cross-common-binutils-2.43.1-2-x86_64.pkg.tar.zst  ./packages/gcc14_toolchain_msys2_mingw64_packages/
+cp -f ./mingw-w64-cygwin-like-cross-binutils/mingw-w64-cygwin-like-cross-mingw64-binutils-2.43.1-2-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_mingw64_packages/
+cp -f ./mingw-w64-cygwin-like-cross-gcc/mingw-w64-cygwin-like-cross-mingw64-gcc-14.2.0-3-x86_64.pkg.tar.zst           ./packages/gcc14_toolchain_msys2_mingw64_packages/
+cp -f ./mingw-w64-cygwin-like-cross-headers/mingw-w64-cygwin-like-cross-mingw64-headers-12.0.0.r250.gc6bf4bdf6-1-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_mingw64_packages/
+cp -f ./mingw-w64-cygwin-like-cross-crt/mingw-w64-cygwin-like-cross-mingw64-crt-12.0.0.r250.gc6bf4bdf6-1-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_mingw64_packages/
+cp -f ./mingw-w64-cygwin-like-cross-winpthreads/mingw-w64-cygwin-like-cross-mingw64-winpthreads-12.0.0.r250.gc6bf4bdf6-1-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_mingw64_packages/
+cp -f ./mingw-w64-cygwin-like-cross-windows-default-manifest/mingw-w64-cygwin-like-cross-mingw64-windows-default-manifest-6.4-5-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_mingw64_packages/
+
+mkdir -p packages/gcc14_toolchain_msys2_mingw32_packages
+
+rm -f ./packages/gcc14_toolchain_msys2_mingw32_packages/*.zst
+cp -f ./mingw-w64-cygwin-like-cross-binutils/mingw-w64-cygwin-like-cross-common-binutils-2.43.1-2-x86_64.pkg.tar.zst  ./packages/gcc14_toolchain_msys2_mingw32_packages/
+cp -f ./mingw-w64-cygwin-like-cross-binutils/mingw-w64-cygwin-like-cross-mingw32-binutils-2.43.1-2-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_mingw32_packages/
+cp -f ./mingw-w64-cygwin-like-cross-gcc/mingw-w64-cygwin-like-cross-mingw32-gcc-14.2.0-3-x86_64.pkg.tar.zst           ./packages/gcc14_toolchain_msys2_mingw32_packages/
+cp -f ./mingw-w64-cygwin-like-cross-headers/mingw-w64-cygwin-like-cross-mingw32-headers-12.0.0.r250.gc6bf4bdf6-1-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_mingw32_packages/
+cp -f ./mingw-w64-cygwin-like-cross-crt/mingw-w64-cygwin-like-cross-mingw32-crt-12.0.0.r250.gc6bf4bdf6-1-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_mingw32_packages/
+cp -f ./mingw-w64-cygwin-like-cross-winpthreads/mingw-w64-cygwin-like-cross-mingw32-winpthreads-12.0.0.r250.gc6bf4bdf6-1-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_mingw32_packages/
+cp -f ./mingw-w64-cygwin-like-cross-windows-default-manifest/mingw-w64-cygwin-like-cross-mingw32-windows-default-manifest-6.4-5-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_mingw32_packages/
+
+mkdir -p packages/gcc14_toolchain_msys2_UCRT64_packages
+
+rm -f ./packages/gcc14_toolchain_msys2_UCRT64_packages/*.zst
+cp -f ./mingw-w64-cygwin-like-cross-binutils/mingw-w64-cygwin-like-cross-common-binutils-2.43.1-2-x86_64.pkg.tar.zst  ./packages/gcc14_toolchain_msys2_UCRT64_packages/
+cp -f ./mingw-w64-cygwin-like-cross-binutils/mingw-w64-cygwin-like-cross-ucrt64-binutils-2.43.1-2-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_UCRT64_packages/
+cp -f ./mingw-w64-cygwin-like-cross-gcc/mingw-w64-cygwin-like-cross-ucrt64-gcc-14.2.0-3-x86_64.pkg.tar.zst           ./packages/gcc14_toolchain_msys2_UCRT64_packages/
+cp -f ./mingw-w64-cygwin-like-cross-headers/mingw-w64-cygwin-like-cross-ucrt64-headers-12.0.0.r250.gc6bf4bdf6-1-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_UCRT64_packages/
+cp -f ./mingw-w64-cygwin-like-cross-crt/mingw-w64-cygwin-like-cross-ucrt64-crt-12.0.0.r250.gc6bf4bdf6-1-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_UCRT64_packages/
+cp -f ./mingw-w64-cygwin-like-cross-winpthreads/mingw-w64-cygwin-like-cross-ucrt64-winpthreads-12.0.0.r250.gc6bf4bdf6-1-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_UCRT64_packages/
+cp -f ./mingw-w64-cygwin-like-cross-windows-default-manifest/mingw-w64-cygwin-like-cross-ucrt64-windows-default-manifest-6.4-5-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_UCRT64_packages/
