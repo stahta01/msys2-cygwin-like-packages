17681a8df91f6fa4826db45c7a1f710f3fa3188f
Tim Stahlhut <stahta01@gmail.com>
2024-10-14 13:26:41 -0400

    script: Update copy-msys2-mingw-gcc-14-toolchain-packages.sh

--- copy-msys2-mingw-gcc-14-toolchain-packages.sh
+++ copy-msys2-mingw-gcc-14-toolchain-packages.sh
@@ -30,3 +30,13 @@ cp -f ./mingw-w64-cygwin-like-cross-headers/mingw-w64-cygwin-like-cross-ucrt64-h
 cp -f ./mingw-w64-cygwin-like-cross-crt/mingw-w64-cygwin-like-cross-ucrt64-crt-12.0.0.r250.gc6bf4bdf6-1-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_UCRT64_packages/
 cp -f ./mingw-w64-cygwin-like-cross-winpthreads/mingw-w64-cygwin-like-cross-ucrt64-winpthreads-12.0.0.r250.gc6bf4bdf6-1-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_UCRT64_packages/
 cp -f ./mingw-w64-cygwin-like-cross-windows-default-manifest/mingw-w64-cygwin-like-cross-ucrt64-windows-default-manifest-6.4-5-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_UCRT64_packages/
+
+mkdir -p packages/gcc14_toolchain_msys2_meta_packages
+
+rm -f ./packages/gcc14_toolchain_msys2_meta_packages/*.zst
+cp -f ./mingw-w64-cygwin-like-cross-binutils/mingw-w64-cygwin-like-cross-binutils-2.43.1-2-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_meta_packages/
+cp -f ./mingw-w64-cygwin-like-cross-gcc/mingw-w64-cygwin-like-cross-gcc-14.2.0-3-x86_64.pkg.tar.zst           ./packages/gcc14_toolchain_msys2_meta_packages/
+cp -f ./mingw-w64-cygwin-like-cross-headers/mingw-w64-cygwin-like-cross-headers-12.0.0.r250.gc6bf4bdf6-1-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_meta_packages/
+cp -f ./mingw-w64-cygwin-like-cross-crt/mingw-w64-cygwin-like-cross-crt-12.0.0.r250.gc6bf4bdf6-1-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_meta_packages/
+cp -f ./mingw-w64-cygwin-like-cross-winpthreads/mingw-w64-cygwin-like-cross-winpthreads-12.0.0.r250.gc6bf4bdf6-1-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_meta_packages/
+cp -f ./mingw-w64-cygwin-like-cross-windows-default-manifest/mingw-w64-cygwin-like-cross-windows-default-manifest-6.4-5-x86_64.pkg.tar.zst ./packages/gcc14_toolchain_msys2_meta_packages/
